38e41cc7ea6e42d5c5268259813d82ef03e7e1d2
h2danilo <h2danilo@hotmail.com>
2022-11-22 10:22:43 -0300

    Main

---
+++ Certificado/Controllers/CertificateController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Certificado.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CertificateController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Certificate()
+        {
+            string pem = @"
+-----BEGIN CERTIFICATE-----
+MIIE8DCCAtigAwIBAgICBnowDQYJKoZIhvcNAQELBQAwgdIxCzAJBgNVBAYTAkJS
+MRIwEAYDVQQIDAlTYW8gUGF1bG8xEjAQBgNVBAcMCVNhbyBQYXVsbzEgMB4GA1UE
+CgwXUGFnU2VndXJvIEludGVybmV0IFMuQS4xIDAeBgNVBAsMF1BhZ1NlZ3VybyBJ
+bnRlcm5ldCBTLkEuMSkwJwYDVQQDDCBzZWN1cmUuc2FuZGJveC5hcGkucGFnc2Vn
+dXJvLmNvbTEsMCoGCSqGSIb3DQEJARYdbC1wYWdzZWd1cm8tYmVybGltQHVvbGlu
+Yy5jb20wHhcNMjIxMTA5MjAxODAzWhcNMzIxMTA5MjAxODAzWjBgMQswCQYDVQQG
+EwJCUjETMBEGA1UECAwKU8OjbyBQYXVsbzETMBEGA1UEBwwKU8OjbyBQYXVsbzEn
+MCUGA1UEChMeRmVsaXBlIEFsZXNzYW5kZXIgQ2FldGFubyBMdWl6MIIBIjANBgkq
+hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnFuaNmNuYPTyoiy3OU6fQLknsVq5+s6b
+oB1fjKghXuS917wLmgZX9OjBsCvC8G5F32ICLtTRg1m0NzfIfSklKq8LTOiyb2a6
+LAzQ4y+ynl3/fGbr0iMCg7P2cWrpoagzXDpZcqAZLvgOPFhFrkEz0Hdgf7/nkdo2
+CB77R4FMWW2Sena3pSiMdGqL7tfjowJNirNOhQkGfUJAJgnc78HreQXWCdapHgen
+JQkAqRW4mVU0Bv9+cWbhDVNPtYasxdIKlfoUfzNagFcc6TY054Tlb1lHn7NgMcOO
+4B/VK3064AxrPIkKrt1xw7sAMdTvxjumZNONKp1AF1HM1tyXfLTEcQIDAQABo0Ew
+PzAOBgNVHQ8BAf8EBAMCB4AwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMB
+MA4GA1UdDgQHBAUBAgMEBjANBgkqhkiG9w0BAQsFAAOCAgEA24gDqXkiD5wDSRDS
+ZJC57onXjqO+Db+q4imDxo5a5cgeAvS+lzzJv6+BearXRc6EeY3LaX3eoDoHAbki
+GrEgeR7+ZvfD43pSeFdTOQaEBVlsdKhCrXVkVSshsEvQ9+jtehQ0rlE8f377v8kr
+fI8z3sqDiVjivx6Z0Hb76vvBABDz/hzejLOiVnYTCi6z54sNAjTlqZHIPrin8BWB
+fb88Hdz1PAZ+xmMFoTog/z5fwhPhbA7WJe+uNl5WYAndz8+5yul4A1Yi0Y9VW04e
+mADNzKE+K+8vApn7Ah63rDSOghHNiFrNBvaVZ8g3h8mpIDRrtvJG4gLjmnE929uS
+HaY1VJhYsRcDMdJwx63bRDiYL4V3rhAWGsyGryMYaX1GnKv9p9GhoZnxJVr+wOsy
+L+nrVKEllujsT975tK+JxLti7fC99N4Rr6bXZ0O3p1wZ46pzV1YD2GlTgJtpY00i
+3H2JXNG8m3tlCnLMI57s+Er+Z4aBCTpglGV2/kJDabJdYPwRhUz3oyAZsv2pgCeG
+Q8B5OAnmLVfA8Kw80jMW24oCsAnqU3FKHqxuSI64zwlAWtUjY84wh5yvjCBryHXn
+OLR18is/iKut2Jexb7XOmnSl/rUssqYwAmkTGepFFPx5ZSWALMfhz1nlFEyCqL7S
+Sw8xcFGADL4HoHl88465yLXNKvg=
+-----END CERTIFICATE-----
+";
+
+            string prikey = @"-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAnFuaNmNuYPTyoiy3OU6fQLknsVq5+s6boB1fjKghXuS917wL
+mgZX9OjBsCvC8G5F32ICLtTRg1m0NzfIfSklKq8LTOiyb2a6LAzQ4y+ynl3/fGbr
+0iMCg7P2cWrpoagzXDpZcqAZLvgOPFhFrkEz0Hdgf7/nkdo2CB77R4FMWW2Sena3
+pSiMdGqL7tfjowJNirNOhQkGfUJAJgnc78HreQXWCdapHgenJQkAqRW4mVU0Bv9+
+cWbhDVNPtYasxdIKlfoUfzNagFcc6TY054Tlb1lHn7NgMcOO4B/VK3064AxrPIkK
+rt1xw7sAMdTvxjumZNONKp1AF1HM1tyXfLTEcQIDAQABAoIBAQCCWauapBIlKpAM
+u5KUaANEBhgdNzHM8emz9kjuDkyju7a438AcTCWPu2ud3x3JK2V88Uw4lpuEEYi4
+b/zouyCNERkuqN/5F38lshmDDQSwGlUf9P2sVhuGiMVvkF3tszf+L3bapLXZ/lp/
+3Gda+mij5qHAXtBDO9PHuWxrFFP+SJk1yjHmXVAuBUsfpOZh4yE0QCwbWPA6gvwJ
+BM9lcJR0FMRc24XxuPg+mSNcaOCZHcpH+GoOogSFCxfGkHSXUSO4HfVKJnfj1/tX
+xGT5v2S+oGwpxJZ3lFKRUIoO8GG9a0n1pQP/fCoB4HIg1AsgU5dc64vWchmYaXBD
+fwrFx2TxAoGBAMpEjK+6Xd4k3P87qFFoG+PC7P/VEs/baBTtOXlSz4r48b4/Nwzl
+8RxM3LuNCw7gb/6V6p4EPLxbhVnJeHx8oWfTyugYhlQYXHT22hi2ZX37+gobCJin
+Iqm39ieXd9Bfn89O/Fe9i5GCK/kwduhOEC++VFcNnQbGap/seyB1hqxTAoGBAMXk
+5PQHbvtmj7yxwuTx94xADkacmJ3ivdGT70sIDwZidcZ66dD4/PM0ANqSTc/HQmqp
+UHepcHMKm2PplJMFX11ZpqrRHTTWq9lL0VIMB7PHPO3Q6C2JMOZ625A69yZnGUPt
+x4EfnMNO/EHLMS3jpMiIYh6x5hQLcuoOwzuEhJOrAoGAO9shTvd1w8Fg7N74pm6K
+olIS1Hp3gK9LE7ZH5dFNvbCByWznm5eITHmqcI4Ckbp1Kf+vWGekmZDz22g1kZFI
+KnOQfjKTvdsfsTVDwUBotGmWytrJJnN/XH4mB/1Y5X8GydBz+HsNDFhbLvjal1BS
+le5bAw/yQgkBkKXooehlnCUCgYBnAojzRdNNZcu4S0fJ6PIUvVsxAl6nANoR99F9
+2woHJsVrtZt0PcddyVoQbEu4tl4qK0xJnZd9Kq5MSdAe35MOgpYxIHz1fjEiAUdK
+UBzKRhG/MRlKarlbn+QepbL0+IG8TE+nNQ9i0wCYNu64lmC2SdfZZnz/Ph5pG5og
+pxBj1QKBgBUHWMCjn3x7hpikCGOcXVKKc2+ETDMduiMdHnvj6fvHzZ9vcraDYK9n
+h4s9btSzmYLky550h7QgB3u19xqXF5e8hsEWSanQ2nDr9zb9DL8XoCa4sOXZWMNz
+vwkcac2/UhvRTSBtaSjW502ouKoIuZcRsKXozFdvKl3bo3CfOqCp
+-----END RSA PRIVATE KEY-----";
+
+
+            try
+            {
+                var certnew = GetCertificateFromPEMstring(pem, prikey, "");
+                return Ok(certnew);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }            
+        }
+
+        public static X509Certificate2 GetCertificateFromPEMstring(string publicCert, string privateKey, string password)
+        {
+            byte[] certBuffer = Helpers.GetBytesFromPEM(publicCert, PemStringType.Certificate);
+            byte[] keyBuffer = Helpers.GetBytesFromPEM(privateKey, PemStringType.RsaPrivateKey);
+
+            X509Certificate2 certificate = new X509Certificate2(certBuffer, password);
+
+
+            RSACryptoServiceProvider prov = Crypto.DecodeRsaPrivateKey(keyBuffer);
+            certificate.PrivateKey = prov;
+
+            return certificate;
+        }
+    }
+}
---
+++ Certificado/Crypto.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Certificado
+{
+    public class Crypto
+    {
+        /// <summary>
+        /// This helper function parses an RSA private key using the ASN.1 format
+        /// </summary>
+        /// <param name="privateKeyBytes">Byte array containing PEM string of private key.</param>
+        /// <returns>An instance of <see cref="RSACryptoServiceProvider"/> rapresenting the requested private key.
+        /// Null if method fails on retriving the key.</returns>
+        public static RSACryptoServiceProvider DecodeRsaPrivateKey(byte[] privateKeyBytes)
+        {
+            MemoryStream ms = new MemoryStream(privateKeyBytes);
+            BinaryReader rd = new BinaryReader(ms);
+
+            try
+            {
+                byte byteValue;
+                ushort shortValue;
+
+                shortValue = rd.ReadUInt16();
+
+                switch (shortValue)
+                {
+                    case 0x8130:
+                        // If true, data is little endian since the proper logical seq is 0x30 0x81
+                        rd.ReadByte(); //advance 1 byte
+                        break;
+                    case 0x8230:
+                        rd.ReadInt16();  //advance 2 bytes
+                        break;
+                    default:
+                        Debug.Assert(false);     // Improper ASN.1 format
+                        return null;
+                }
+
+                shortValue = rd.ReadUInt16();
+                if (shortValue != 0x0102) // (version number)
+                {
+                    Debug.Assert(false);     // Improper ASN.1 format, unexpected version number
+                    return null;
+                }
+
+                byteValue = rd.ReadByte();
+                if (byteValue != 0x00)
+                {
+                    Debug.Assert(false);     // Improper ASN.1 format
+                    return null;
+                }
+
+                // The data following the version will be the ASN.1 data itself, which in our case
+                // are a sequence of integers.
+
+                // In order to solve a problem with instancing RSACryptoServiceProvider
+                // via default constructor on .net 4.0 this is a hack
+                CspParameters parms = new CspParameters();
+                parms.Flags = CspProviderFlags.NoFlags;
+                parms.KeyContainerName = Guid.NewGuid().ToString().ToUpperInvariant();
+                parms.ProviderType = ((Environment.OSVersion.Version.Major > 5) || ((Environment.OSVersion.Version.Major == 5) && (Environment.OSVersion.Version.Minor >= 1))) ? 0x18 : 1;
+
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(parms);
+                RSAParameters rsAparams = new RSAParameters();
+
+                rsAparams.Modulus = rd.ReadBytes(Helpers.DecodeIntegerSize(rd));
+
+                // Argh, this is a pain.  From emperical testing it appears to be that RSAParameters doesn't like byte buffers that
+                // have their leading zeros removed.  The RFC doesn't address this area that I can see, so it's hard to say that this
+                // is a bug, but it sure would be helpful if it allowed that. So, there's some extra code here that knows what the
+                // sizes of the various components are supposed to be.  Using these sizes we can ensure the buffer sizes are exactly
+                // what the RSAParameters expect.  Thanks, Microsoft.
+                RSAParameterTraits traits = new RSAParameterTraits(rsAparams.Modulus.Length * 8);
+
+                rsAparams.Modulus = Helpers.AlignBytes(rsAparams.Modulus, traits.size_Mod);
+                rsAparams.Exponent = Helpers.AlignBytes(rd.ReadBytes(Helpers.DecodeIntegerSize(rd)), traits.size_Exp);
+                rsAparams.D = Helpers.AlignBytes(rd.ReadBytes(Helpers.DecodeIntegerSize(rd)), traits.size_D);
+                rsAparams.P = Helpers.AlignBytes(rd.ReadBytes(Helpers.DecodeIntegerSize(rd)), traits.size_P);
+                rsAparams.Q = Helpers.AlignBytes(rd.ReadBytes(Helpers.DecodeIntegerSize(rd)), traits.size_Q);
+                rsAparams.DP = Helpers.AlignBytes(rd.ReadBytes(Helpers.DecodeIntegerSize(rd)), traits.size_DP);
+                rsAparams.DQ = Helpers.AlignBytes(rd.ReadBytes(Helpers.DecodeIntegerSize(rd)), traits.size_DQ);
+                rsAparams.InverseQ = Helpers.AlignBytes(rd.ReadBytes(Helpers.DecodeIntegerSize(rd)), traits.size_InvQ);
+
+                rsa.ImportParameters(rsAparams);
+                return rsa;
+            }
+            catch (Exception)
+            {
+                Debug.Assert(false);
+                return null;
+            }
+            finally
+            {
+                rd.Close();
+            }
+        }
+    }
+
+    internal class RSAParameterTraits
+    {
+        public RSAParameterTraits(int modulusLengthInBits)
+        {
+            // The modulus length is supposed to be one of the common lengths, which is the commonly referred to strength of the key,
+            // like 1024 bit, 2048 bit, etc.  It might be a few bits off though, since if the modulus has leading zeros it could show
+            // up as 1016 bits or something like that.
+            int assumedLength = -1;
+            double logbase = Math.Log(modulusLengthInBits, 2);
+            if (logbase == (int)logbase)
+            {
+                // It's already an even power of 2
+                assumedLength = modulusLengthInBits;
+            }
+            else
+            {
+                // It's not an even power of 2, so round it up to the nearest power of 2.
+                assumedLength = (int)(logbase + 1.0);
+                assumedLength = (int)(Math.Pow(2, assumedLength));
+                System.Diagnostics.Debug.Assert(false);  // Can this really happen in the field?  I've never seen it, so if it happens
+                // you should verify that this really does the 'right' thing!
+            }
+
+            switch (assumedLength)
+            {
+                case 1024:
+                    this.size_Mod = 0x80;
+                    this.size_Exp = -1;
+                    this.size_D = 0x80;
+                    this.size_P = 0x40;
+                    this.size_Q = 0x40;
+                    this.size_DP = 0x40;
+                    this.size_DQ = 0x40;
+                    this.size_InvQ = 0x40;
+                    break;
+                case 2048:
+                    this.size_Mod = 0x100;
+                    this.size_Exp = -1;
+                    this.size_D = 0x100;
+                    this.size_P = 0x80;
+                    this.size_Q = 0x80;
+                    this.size_DP = 0x80;
+                    this.size_DQ = 0x80;
+                    this.size_InvQ = 0x80;
+                    break;
+                case 4096:
+                    this.size_Mod = 0x200;
+                    this.size_Exp = -1;
+                    this.size_D = 0x200;
+                    this.size_P = 0x100;
+                    this.size_Q = 0x100;
+                    this.size_DP = 0x100;
+                    this.size_DQ = 0x100;
+                    this.size_InvQ = 0x100;
+                    break;
+                default:
+                    System.Diagnostics.Debug.Assert(false); // Unknown key size?
+                    break;
+            }
+        }
+
+        public int size_Mod = -1;
+        public int size_Exp = -1;
+        public int size_D = -1;
+        public int size_P = -1;
+        public int size_Q = -1;
+        public int size_DP = -1;
+        public int size_DQ = -1;
+        public int size_InvQ = -1;
+    }
+}
